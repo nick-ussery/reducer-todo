871064e5aa406459894b03fdd07f8ea3654048a9
nick-ussery <nick.ussery@gmail.com>
2020-06-09 00:16:55 -0500

    fixed completed

--- todo/src/components/todo.js
+++ todo/src/components/todo.js
@@ -1,44 +1,62 @@
-import React, {useState, useReducer} from 'react';
+import React, {useState, useReducer, useEffect} from 'react';
 import {reducer, initialState} from '../reducer/reducer'
 
+
+
+
+
 const Todo = () =>{
+    const [state, dispatch] = useReducer(reducer, initialState);
     const [values, setValues] = useState('')
     const handleChanges = e =>{
         setValues(e.target.value)
     }
-
     const addTodo = (e) => {
         e.preventDefault();
-        e.stopPropogation()
         // console.log('button clicked')
         dispatch({type: 'ADD_TODO', payload: values})
     }
 
-
-
-    const [state, dispatch] = useReducer(reducer, initialState);
-    // console.log('state',state)
+    // console.log('state before mapping', state.todos)
+    const [list, setList] = useState(state.todos.map(todo=>{
+        // console.log('todo given to mapping', todo)
+        return (<div style={(todo.completed===true) ? {marginLeft: 'auto', marginRight: 'auto',width: '500px',border: '1px solid black',background:'red'} : {marginLeft: 'auto', marginRight: 'auto',width:'500px',border: '1px solid black',background:'white'}} key={todo.id}>
+            <p>{todo.item}</p>
+                completed: {todo.completed.toString()}
+                <button onClickCapture= {e =>{                            
+                    e.preventDefault()
+                    e.bubbles = false
+                    dispatch({
+                    type: 'TOGGLE_COMPLETED',
+                    payload: todo.id
+                })
+                }}>Completed?</button>
+        </div>)}))
+    // console.log('state',list)
+    useEffect(()=>{
+        if(!state.todos){return }
+        setList(
+        state.todos.map(todo=>{
+            // console.log('todo given to mapping', todo)
+            return (<div style={(todo.completed===true) ? {marginLeft: 'auto', marginRight: 'auto',width: '500px',border: '1px solid black',background:'red'} : {marginLeft: 'auto', marginRight: 'auto',width:'500px',border: '1px solid black',background:'white'}} key={todo.id}>
+                <p>{todo.item}</p>
+                    completed: {todo.completed.toString()}
+                    <button onClickCapture= {e =>{                            
+                        e.preventDefault()
+                        e.bubbles = false
+                        dispatch({
+                        type: 'TOGGLE_COMPLETED',
+                        payload: todo.id
+                    })
+                    }}>Completed?</button>
+            </div>)}))
+    }
+    
+    ,[state])
 
     return (
         <div style= {{marginLeft: 'auto', marginRight: 'auto',}}>
-            {state.map(todo =>{
-                return(
-                    <div style={(todo.completed===true) ? {marginLeft: 'auto', marginRight: 'auto',width: '500px',border: '1px solid black',background:'red'} : {marginLeft: 'auto', marginRight: 'auto',width:'500px',border: '1px solid black',background:'white'}} key={todo.id}>
-                        <p>{todo.item}</p>
-                        completed: {todo.completed.toString()}
-                        <button onClickCapture= {e =>{
-                            e.stopPropagation()
-                            e.preventDefault()
-                            e.bubbles = false
-                            dispatch({
-                                type: 'TOGGLE_COMPLETED',
-                                payload: todo.id
-                            })
-                        }}>Completed?</button>
-
-                    </div>
-                )
-            })}
+            {list}
 
             <input 
             type='text'
--- todo/src/reducer/reducer.js
+++ todo/src/reducer/reducer.js
@@ -1,39 +1,52 @@
 // import React from 'react';
 import {v4 as uuid} from 'uuid';
 
-export const initialState = [{
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-}]
+export const initialState = {
+    todos:[{
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },{
+        item: 'Task 1',
+        completed: false,
+        id: uuid()
+    }]
+}
 
 export const reducer = (state, action) =>{
     switch(action.type)    {
         case 'CLEAR_COMPLETED':
-            return state.filter(task=>{
-                return task.completed === false
-            })
-        case 'TOGGLE_COMPLETED':
 
-            let updatedTask = state.filter(task=>{return task.id === action.payload})            
-            updatedTask = updatedTask[0]
-            let index = state.indexOf(updatedTask)
-            console.log('index of updated task', index)
-            updatedTask.completed = !updatedTask.completed
-             return[
-                 ...state,
-                 state[index].completed = !state[index].completed
-             ]
-
-        case 'ADD_TODO':
-            return[
+            return{
                 ...state,
+                todos:{
+                    ...state.todos.filter(todo=>{
+                        return todo.completed === false
+                    })
+                }
+            }
+        case 'TOGGLE_COMPLETED':
+            // console.log('toggle start')
+
+            return {
+                ...state,
+                todos: state.todos.map(todo=>{
+                    if(todo.id === action.payload){
+                        return {...todo, completed: !todo.completed}
+                    }else{ return {...todo}}
+                })
+            }
+            
+        case 'ADD_TODO':
+            return{
+                ...state,
+                todos:[...state.todos, 
                 {
                     item: action.payload,
                     completed: false,
                     id: uuid()
-                }
-            ]
+                }]
+    }
         default:
             return state
     }
